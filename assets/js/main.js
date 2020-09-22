a397afec9181205c6e5e554cc92c923d85a46a5b
emanmoq <42995029+emanmoq@users.noreply.github.com>
2020-09-22 21:48:11 +0300

    xd

--- assets/js/main.js
+++ assets/js/main.js
@@ -63,8 +63,11 @@ jQuery(function ($) {
                 items: 2,
               },
               1000: {
-                items: 4,
+                items: 3,
         
+              },
+              1200:{
+                items: 3,
               }
          
             }
